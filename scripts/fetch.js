c1e3892176b60eb22b31cf12a7c30aae0d46fb67
Nathan Duker <npd5889@nyu.edu>
2024-04-16 11:00:32 -0400

    Building an Online PokeDex App

---
+++ scripts/fetch.js
@@ -0,0 +1,63 @@
+// Specify the API endpoint for user data
+const pokemonNames = [
+    "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard",
+    "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree",
+    "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot",
+    "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok",
+    "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina",
+    "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable",
+    "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat",
+    "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat",
+    "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck",
+    "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag",
+    "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop",
+    "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool",
+    "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash",
+    "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo",
+    "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder",
+    "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee",
+    "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute",
+    "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung",
+    "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela",
+    "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu",
+    "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar",
+    "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto",
+    "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte",
+    "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno",
+    "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo",
+    "Mew"
+];
+const lowercasePokemonNames = pokemonNames.map(name => name.toLowerCase());
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/'+lowercasePokemonNames[0]+'/';
+const pkListNumber = pokemonNames.length; //Number of pokemon found in the array
+let pkRandNumber = 0;
+
+const submitButton = document.getElementById('change');
+const changeText = document.getElementById('changeText')
+
+// Make a GET request using the Fetch API
+
+function genPk(){
+  fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(pkData => {
+    // Process the retrieved user data
+    console.log('Pokemon Data:', pkData);
+    console.log(pkData.sprites.front_default)
+    console.log('Pokemon Names Length: ' + pokemonNames.length);
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+function randomizePk(){
+  pkRandNumber = Math.floor(Math.random() * 152);
+  console.log('Pokemon Random Number: ' + pkRandNumber );
+}
